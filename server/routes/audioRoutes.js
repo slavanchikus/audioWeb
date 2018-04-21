945426c4f145799870306a613bead29f2fed3963
home <varchik123@yandex.ru>
2018-04-21 14:38:14 +0300

    also trying

--- server/routes/audioRoutes.js
+++ server/routes/audioRoutes.js
@@ -1,5 +1,7 @@
 const https = require('https');
 
+const coolCookue = 'remixlang=0; remixstid=94762466_82231675fbc5b0cacf; remixflash=0.0.0; remixscreen_depth=24; remixdt=0; remixmdevice=1920/1200/1/!!-!!!!; remixgp=0a43b349c2a6b49ad35fd6f0d76db23a; remixseenads=2; remixsid=e79beb7e417f0e05c7e18f328c9bdf33512d985d62e8754af9f5b; remixrefkey=d1fb7610a21ed037de; remixcurr_audio=9387646_456239111';
+
 module.exports = function(app) {
   app.post('/getaudio', (mainReq, mainRes) => {
     const params = mainReq.body.params;
